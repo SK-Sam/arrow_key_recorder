a4a36750cfb870841380324a46660154c10262c5
Sam <yeosamuel95@gmail.com>
2020-03-14 05:17:50 -0400

    Added pseudocode

---
+++ arrow_key.py
@@ -0,0 +1,13 @@
+#1: Have the user type into the terminal with arrow keys.
+#2: Find out which arrow key the user typed.
+#3: Each different arrow key will result to a different text response from the terminal.
+#4: If the Arrow key is UP, the terminal will print "UP", if LEFT key, print "LEFT"
+    #if RIGHT key, print "RIGHT", if DOWN key, print "DOWN".
+#5:Make sure the program runs continuously until the user decides to stop it
+    #-The user should type a specific key to stop the program from infinitely asking them to type.
+#6:Make sure each time the user types a key in, the terminal will add a line in between each time it prints out a key.
+#7:If the user presses the 'r' key, the program will begin to "record" all of the keys pressed.
+#8:Make a message that clearly shows the user the program is "recording" and stops "recording".
+#9:The recording will stop once the user presses 'r' again to escape the "record" mode.
+#10:Make a message that clearly shows the program is about to "play" the "recorded" messages.
+#11:The user can then press p key to print every recorded key onto the terminal.
